dafe016540cfa9d0c059ab2f161c0a8fa73f9c5f
inventans <gerson@inventans.com>
2015-07-22 00:14:17 -0300

    Melhorias
    Tratamento de maisculas e minusculas na resposta. Limpeza do campo
    resposta ao clicar.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,5 @@
+var resposta = /Roma/i;
+
 //GET /quizes/question
 exports.question = function(req, res){
  res.render('quizes/question',{pregunta: 'Capital de Italia'});
@@ -5,7 +7,7 @@ exports.question = function(req, res){
 
 //GET /quizes/answer
 exports.answer = function(req,res){
-  if (req.query.respuesta === 'Roma'){
+  if (resposta.test(req.query.respuesta)){
     res.render('quizes/answer', {respuesta:'Correcto'});
   } else {
     res.render('quizes/answer', {respuesta:'Incorrecto'});
