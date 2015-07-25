cf62d38dac79772e3521fb247a68c14a39ea767d
inventans <gerson@inventans.com>
2015-07-25 17:40:34 -0300

    Despliegue a Heroku

--- models/models.js
+++ models/models.js
@@ -1,30 +1,52 @@
 var path = require('path');
+
+//Postgres DATABASE_URL = postgres://user:passswd@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6]||null);
+var user      = (url[2]||null);
+var pwd       = (url[3]||null);
+var protocol  = (url[1]||null);
+var dialect   = (url[1]||null);
+var port      = (url[5]||null);
+var host      = (url[4]||null);
+var storage   = process.env.DATABASE_STORAGE;
+
+//Cargar Modelo ORM
+
 var Sequelize = require('sequelize');
 
-
-//Cargar modelo ORM
+//Usar BBDD SQLite o PostGres
 var sequelize = new Sequelize(
-  null,
-  null,
-  null,
-  {dialect: "sqlite", storage:"quiz.sqlite"}
+    DB_name,
+    user,
+    pwd,
+  {
+    dialect: protocol,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage, //solo SQLite (.env)
+    omitNull: true // solo Postgres
+  }
 );
 
-//importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+//importar la definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; //exportar la definicion de tabla quiz
+exports.Quiz = Quiz; //exportar la tabla quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
-  //success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+  //then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count){
     if(count===0){
       Quiz.create({
         pregunta:'Capital de Italia',
         respuesta:'Roma'
       })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
